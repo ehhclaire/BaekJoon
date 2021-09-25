90ca4f8329f05ac59e54470e3610415320439c40
bee1404 <bee1404@naver.com>
2021-09-25 14:59:17 +0900

    boj 15650 S3 N과  M(2) Solution

---
+++ backtracking/Main_S3_15650.java
@@ -0,0 +1,34 @@
+package boj.Silver;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main_S3_15650 {
+	static int n;
+	static int r;
+	static int[] numbers;
+	
+	private static void combination(int start, int cnt) {
+		if(cnt==r) {
+			for(int num : numbers) {
+				System.out.print(num + " ");
+			}
+			System.out.println();
+			return;
+		}
+			
+		for(int i=start; i<=n; i++) {
+			numbers[cnt] = i;
+			combination(i+1, cnt+1);
+		}		
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		r = sc.nextInt();
+		
+		numbers = new int[r];
+		combination(1, 0);
+	}
+}
