d1226c1429807be5fe1167fb92caef9d1ef95ff5
bee1404 <bee1404@naver.com>
2021-10-02 22:00:21 +0900

    boj 15664 S3 N과 M (10) Solution

---
+++ 순조부(순열_조합_부분집합)/Main_S2_15664.java
@@ -0,0 +1,61 @@
+package boj.Silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main_S2_15664 {
+	static int N, M;
+	static int[] inputs;
+	static int[] numbers;
+	static Set<String> set;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+				
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		inputs = new int[N];
+		numbers = new int[M];
+		set = new LinkedHashSet<String>();
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			inputs[i] = Integer.parseInt(st.nextToken());
+		}
+				
+		
+		Arrays.sort(inputs);
+		
+		combination(0, 0);
+		
+		StringBuilder sb2 = new StringBuilder();
+        for(String list : set) {
+            sb2.append(list).append("\n");
+        }
+        System.out.println(sb2);
+
+	}
+
+	// 순열
+	private static void combination(int cnt, int start) {
+		if(cnt==M) {
+			StringBuilder sb = new StringBuilder();
+			for(int i=0; i<M; i++) {
+				sb.append(numbers[i]).append(" ");
+			}
+			set.add(sb.toString());
+			return;
+		}
+		
+		for(int i=start; i<N; i++) {
+			numbers[cnt] = inputs[i];
+			combination(cnt+1, i+1);
+		}
+	}
+}
