a0083eb3160641fa4d3a4c39ee0376e33cc4477e
bee1404 <bee1404@naver.com>
2021-10-02 21:59:36 +0900

    boj 15657 S3 N과 M (8) Solution

---
+++ 순조부(순열_조합_부분집합)/Main_S3_15657.java
@@ -0,0 +1,44 @@
+package boj.Silver;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main_S3_15657 {
+	static int n, r;
+	static int[] numbers;
+	static int[] inputs;
+	static StringBuilder sb;
+
+	private static void combination(int start, int cnt) {
+		if(cnt==r) {
+			for(int num : numbers) {
+				System.out.print(num + " ");
+			}
+			System.out.println();
+			return;
+		}
+			
+		for(int i=start; i<n; i++) {
+			numbers[cnt] = inputs[i];
+			combination(i, cnt+1);
+		}		
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		r = sc.nextInt();
+		
+		numbers = new int[r];
+		inputs = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			inputs[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(inputs);
+		
+		combination(0, 0);
+	}
+
+}
