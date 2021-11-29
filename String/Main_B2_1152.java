09161c821601d8f3233dbd33fd09192b7404e524
bee1404 <bee1404@naver.com>
2021-11-29 21:54:30 +0900

    boj 1152 B2 단어의 개수 Solution

---
+++ String/Main_B2_1152.java
@@ -0,0 +1,15 @@
+package boj.Bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_B2_1152 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		System.out.println(st.countTokens());
+	}
+}
