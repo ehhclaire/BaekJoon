ba682d095d1e45d32c436ad640ef932d5ee8c1d9
HSungHee <bee1404@naver.com>
2022-01-06 08:58:03 +0900

    boj 1747 G5 소수&팰린드롬 Solution

--- brute_force/Main_G5_1747.java
+++ brute_force/Main_G5_1747.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		long N = sc.nextLong();
